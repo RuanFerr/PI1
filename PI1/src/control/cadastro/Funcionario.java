6604c85c60b5ddf08e5e82d9a52b9a4087fc8fc2
RuanFerr <50969627+RuanFerr@users.noreply.github.com>
2019-09-29 12:38:41 -0300

    Add files via upload

---
+++ PI1/src/control/cadastro/Funcionario.java
@@ -0,0 +1,42 @@
+package control.cadastro;
+
+public class Funcionario extends Pessoa{
+    
+    public void cadastrarEquip () {
+    
+    }
+    public void alterarEquip () {
+    //parametro
+    }
+    public void buscarItem () {
+     //parametro
+    }
+    public void deletarItem () {
+     //parametro
+    }
+    public void cadastrarUsuario () {
+    
+    }
+    public void alterarUsuario () {
+    //parametro
+    }
+    public void buscarUsuario () {
+     //parametro
+    }
+    public void deletarUsuario () {
+     //parametro
+    }
+    public void cadastrarReserva () {
+    
+    }
+    public void alterarReserva () {
+    //parametro
+    }
+    public void buscarReserva () {
+    //parametro
+    }
+    public void deletarReserva () {
+     //parametro
+    }
+    
+}
---
+++ PI1/src/control/reserva/Reserva.java
@@ -0,0 +1,36 @@
+package control.reserva;
+
+public class Reserva {
+
+    private int idReserva;
+    private String dataHoraReserva;
+    private String itemReserva;
+
+    public int getIdReserva() {
+        return idReserva;
+    }
+
+    public void setIdReserva(int idReserva) {
+        this.idReserva = idReserva;
+    }
+
+    public String getDataHoraReserva() {
+        return dataHoraReserva;
+    }
+
+    public void setDataHoraReserva(String dataHoraReserva) {
+        this.dataHoraReserva = dataHoraReserva;
+    }
+
+    public String getItemReserva() {
+        return itemReserva;
+    }
+
+    public void setItemReserva(String itemReserva) {
+        this.itemReserva = itemReserva;
+    }
+
+    public void registraDevolucao() {
+        
+    }
+}
